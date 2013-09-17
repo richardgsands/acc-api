85fd70024fb37e79533c0386bde2c96e716ad12b
Matt Collins <mailmattcollins@gmail.com>
2013-09-17 16:41:50 +0100

    commenting on example apps js.

--- static/js/child.js
+++ static/js/child.js
@@ -1,28 +1,44 @@
+//Account id for this child
 var ACCOUNT_ID = "523716b2cd87cad76a000001";
 
+//When the page has loaded
 $(document).ready(function(){
 
+    //Get account info
     BOD.core.getAccount({id: ACCOUNT_ID}).done(function(account){
 
+        //Display balance on screen
         $('.balance .value').html(account.balance);
 
+        //Get current date from account (not real world date!!)
         var current_date = moment.utc(account.current_date);
+
+        //Work out when how many days until the next pocket money
         var tillPocketMoney = 0;
 
         if(current_date.format('d') > account.pocket_money_day){
+            //The next pocket money day is in the next week
             tillPocketMoney = 7 - parseInt(current_date.format('d'), 10);
         }else{
+            //The next pocket money day is this week
             tillPocketMoney = account.pocket_money_day - parseInt(current_date.format('d'), 10);
         }
 
+        //It was paid today so we've got a week ahead
+        if(tillPocketMoney === 0)
+            tillPocketMoney = 7;
+
+        //Display the picket money on screen
         $('.pocket-money .value').html(tillPocketMoney);
 
+        //If we've got them display the account goal name and target (value)
         if(!!account.goal.name)
             $('.goals input[name=name]').val(account.goal.name);
 
         if(!!account.goal.value){
             $('.goals input[name=value]').val(account.goal.value);
 
+            //Calculate how many more weeks of pocket money we'll need to get before we meet target
             var target = account.goal.value;
 
             if(account.balance < target){
@@ -31,6 +47,7 @@ $(document).ready(function(){
 
                 var weeksLeft = Math.ceil(difference / account.pocket_money_amount);
 
+                //Display week count on screen
                 $('.goals .value').html(weeksLeft);
             }
         }
@@ -39,10 +56,13 @@ $(document).ready(function(){
 
     });
 
+    //Update account with goal information from form
     $('.update-account').on('submit', function(e){
 
+        //Stop the page reloading
         e.preventDefault();
 
+        //Prep data
         var data = {
             id: ACCOUNT_ID,
             goal: {
@@ -50,12 +70,14 @@ $(document).ready(function(){
             }
         };
 
+        //Get form values and store in data.goal object
         $(this).find('input,select').each(function() {
             data.goal[$(this).attr('name')] = $(this).val();
         });
 
+        //Do API call
         BOD.core.updateAccount(data, function(data){
-            console.log('UPDATED');
+            //Update successful
         });
     });
 });
--- static/js/dad.js
+++ static/js/dad.js
@@ -1,11 +1,17 @@
+//Account id for this child
 var ACCOUNT_ID = "523716b2cd87cad76a000001";
 
+//When the page has loaded
 $(document).ready(function(){
 
+    //Get account info
     BOD.core.getAccount({id: ACCOUNT_ID}).done(function(account){
 
+        //Display balance on screen
         $('.balance .value').html(account.balance);
 
+
+        //Update values of the account update form
         $('.update-account input[name=loan_rate]').val(account.loan_rate);
         $('.update-account input[name=saving_rate]').val(account.saving_rate);
         $('.update-account input[name=pocket_money_amount]').val(account.pocket_money_amount);
@@ -13,58 +19,73 @@ $(document).ready(function(){
 
     });
 
+    //When the account update form is submitted
     $('.update-account').on('submit', function(e){
 
+        //Stop the page reloading
         e.preventDefault();
 
+        //Prep data
         var data = {
             id: ACCOUNT_ID
         };
 
+
+        //Add each form input to data object
         $(this).find('input,select').each(function() {
             data[$(this).attr('name')] = $(this).val();
         });
 
+        //Do API call
         BOD.core.updateAccount(data, function(data){
-            console.log('UPDATED');
+            //successfully updated
         });
     });
 
+    //When the deposit create form is submitted
     $('.create-deposit').on('submit', function(e){
 
+        //Stop the page reloading
         e.preventDefault();
 
+        //Prep data
         var data = {
             account_id: ACCOUNT_ID,
             deposit: true,
             withdrawal: false
         };
 
+        //Add each form input to data object (amount and description)
         $(this).find('input,select').each(function() {
             data[$(this).attr('name')] = $(this).val();
         });
 
+        //Do API call
         BOD.core.createTransaction(data, function(data){
-            console.log('UPDATED');
+            //successfully updated
         });
     });
 
     $('.create-withdrawal').on('submit', function(e){
 
+        //Stop the page reloading
         e.preventDefault();
 
+        //Prep data
         var data = {
             account_id: ACCOUNT_ID,
             deposit: false,
             withdrawal: true
         };
 
+        //Add each form input to data object (amount and description)
         $(this).find('input,select').each(function() {
             data[$(this).attr('name')] = $(this).val();
         });
 
+        //Do API call
         BOD.core.createTransaction(data, function(data){
-            console.log('UPDATED');
+            //successfully updated
         });
     });
 });
